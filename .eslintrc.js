b4e524fdc9f8db17c87adeedf9b2a42ca38a91ee
Sam G <samg@pop-os.localdomain>
2020-07-01 23:47:04 +0300

    Add vuetify config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,33 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        mocha: true,
-      },
+    root: true,
+    env: {
+        node: true,
     },
-  ],
+    extends: [
+        'plugin:vue/recommended',
+        '@vue/airbnb',
+    ],
+    rules: {
+        indent: ['error', 4, {
+            ObjectExpression: 'first',
+            ImportDeclaration: 1,
+        }],
+        'no-tabs': ['error', {
+            allowIndentationTabs: true,
+        }],
+        'vue/html-indent': ['error', 4, {
+            attribute: 1,
+            baseIndent: 1,
+            closeBracket: 0,
+            alignAttributesVertically: true,
+            ignores: [],
+        }],
+        'vue/multiline-html-element-content-newline': 0,
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'import/prefer-default-export': 0,
+    },
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
 };
